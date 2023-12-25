eb9bb8ab31e5809edb0719afc62fb22db898f129
Aerin2805 <aerinpatel28@gmail.com>
2023-12-25 10:52:41 +0530

    Add all changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/bg3MLSA.jpeg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = " 3 laptop image with coding Background ";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Vehical Management System",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "A comprehensive system for tracking, maintaining, and optimizing vehicle fleets.",
+    url: "https://github.com/Aerin2805/AKM_PROJECT",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Crime Control API",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Crime Control API: Empowering crime prevention through data-driven insights and proactive solutions.",
+    url: "https://github.com/Aerin2805/API",
   },
   {
-    title: "My Resume Site",
+    title: "My Resume",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "A proactive and adaptable professional leveraging diverse skills to drive impactful solutions and foster collaborative success.",
+    url: "https://drive.google.com/file/d/11o0dhaJg_SSTfFIzxxZvFpIZ_Vze1kIK/view?usp=sharing",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Bull Stock: Empowering Smart Investments",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Your all-in-one platform for insightful analysis, virtual trading, and confident decision-making in stocks and cryptocurrencies.",
+    url: "https://github.com/Aerin2805/Devheat_Ragnarok-main",
   },
 ];
 
